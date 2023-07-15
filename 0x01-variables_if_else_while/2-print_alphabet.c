4ff9fb78057017979b6549d7587b797896d53ad2
Ahmedmedhat23 <ahmedmedhat404@gmail.com>
2023-07-15 11:59:03 -0700

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+
 /*
- *
  * main - Entry point
  *
  * Description: print all alphabet letters with lower vase letter
  *
  * Return: 0 (Success)
 */
+
 int main(void)
 {
 	char ch = 'a';
 
 	while (ch <= 'z')
-
 	{
 		putchar(ch);
 		ch++;
