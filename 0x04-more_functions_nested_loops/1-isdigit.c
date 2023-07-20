a21289e5091cf1923fabff2b6f769e3961a8a681
Ahmedmedhat23 <ahmedmedhat404@gmail.com>
2023-07-20 09:44:50 -0700

    a

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,14 @@
 /**
  * _isdigit - check if 'c' is digit
  *
- * Description: @c: input for alphabet if digit 'c' is upper or lower
+ * Description: @c: input for alphabet if digit 'c'_isdigit between 0 and 9
  *
  * Return: 1 if _isgigit between 0 and 9, 0 if it not
  */
 
-int _isupper(int c)
+int _isdigit(int c);
 {
+
 	if (c >= 48 && c <= 57)
 		return (1);
 
