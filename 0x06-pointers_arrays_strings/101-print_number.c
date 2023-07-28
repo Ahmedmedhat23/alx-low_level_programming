54482bd742c401ef7d1eba6587c86c8dd77ef6a5
Ahmedmedhat23 <ahmedmedhat404@gmail.com>
2023-07-28 13:07:38 -0700

    a

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,7 +16,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
@@ -24,5 +24,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
